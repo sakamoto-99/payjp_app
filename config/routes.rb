70b951dd4c23530a56f20c1e9b4d9f6601a56fdd
sakamoto-99 <sakamoto.engineer.99@gmail.com>
2021-06-22 13:07:21 +0900

    カード登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   get 'users/show'
   get 'items/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
 end
